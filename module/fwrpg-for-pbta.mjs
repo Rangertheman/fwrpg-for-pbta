6ed90abdf1e78d6df720db20a74a2d342c763891
Rangertheman <81484515+Rangertheman@users.noreply.github.com>
2023-01-08 13:54:15 +0100

    0.0.9

--- module/fwrpg-for-pbta.mjs
+++ module/fwrpg-for-pbta.mjs
@@ -1,29 +1,31 @@
-import { configSheet } from "./helper/config-sheet.mjs"
+import {
+   configSheet
+} from "./helper/config-sheet.mjs"
 
 // once the game has initialized, set up the module
 Hooks.once('init', () => {
 
-  // register FWRPG settings
-  game.settings.register('fwrpg-for-pbta', 'settings-override', {
-    name: game.i18n.localize("fwrpg.Settings.Title"),
-    default: false,
-    type: Boolean,
-    scope: 'world',
-    config: true,
-    hint: game.i18n.localize("fwrpg.Settings.Hint"),
-    onChange: () => setTimeout(() => {
-        location.reload();
+   // register FWRPG settings
+   game.settings.register('fwrpg-for-pbta', 'settings-override', {
+      name: game.i18n.localize("fwrpg.Settings.Title"),
+      default: false,
+      type: Boolean,
+      scope: 'world',
+      config: true,
+      hint: game.i18n.localize("fwrpg.Settings.Hint"),
+      onChange: () => setTimeout(() => {
+         location.reload();
       }, 500)
-  });
+   });
 
 })
 
 Hooks.once('pbtaSheetConfig', () => {
-  
-  // Disable the sheet config form.
-  game.settings.set('pbta', 'sheetConfigOverride', true);
-  
-  // Replace the game.pbta.sheetConfig with FWRPG version.
-  configSheet();
+
+   // Disable the sheet config form.
+   game.settings.set('pbta', 'sheetConfigOverride', true);
+
+   // Replace the game.pbta.sheetConfig with FWRPG version.
+   configSheet();
 
 });
