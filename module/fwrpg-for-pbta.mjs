1acaae567154ef975a93db72286135336f4791fa
Rangertheman <persson.krister@gmail.com>
2023-06-25 12:15:26 +0200

    0.1.3

--- module/fwrpg-for-pbta.mjs
+++ module/fwrpg-for-pbta.mjs
@@ -1,10 +1,40 @@
-import {
-   configSheet
-} from "./helper/config-sheet.mjs"
+import { configSheet } from "./helper/config-sheet.mjs";
+import { PbtaActorSheet } from "../../../systems/pbta/module/actor/actor-sheet.js";
 
+export class FwrpgPbtASheet extends PbtaActorSheet {
+   #dataPath = "system";
+   #shortPath = "system";
+
+   constructor(data, context) {
+       super(data, context);
+
+       this.labelShiftDown = "none";
+       this.labelShiftUp = "none";
+   }
+
+   get template() {
+       //Decision making based on permission level
+       let versionDirectory = "templates";
+       let sheetTemplate = `modules/fwrpg-for-pbta/${versionDirectory}/actor-sheet.hbs`;
+       if (!this.isOwner && !this.isEditable) {
+           //observer, or limited?
+           if (this.actor.permission === CONST.DOCUMENT_PERMISSION_LEVELS.LIMITED) {
+               sheetTemplate = `modules/fwrpg-for-pbta/${versionDirectory}/actor-sheet.hbs`;
+           }
+       }
+       return sheetTemplate;
+      }
+   }
 // once the game has initialized, set up the module
 Hooks.once('init', () => {
 
+   //Register Actor sheet for character (NPC uses default for now)
+   Actors.registerSheet("pbta", FwrpgPbtASheet, {
+      types: ["character"],
+      makeDefault: true
+  });
+
+   
    // register FWRPG settings
    game.settings.register('fwrpg-for-pbta', 'settings-override', {
       name: game.i18n.localize("fwrpg.Settings.Title"),
